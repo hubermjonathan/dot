925ccf3932548e17c9195e823b82032c362c1cd0
Jon Huber <hubermjonathan@gmail.com>
2022-11-10 04:23:23 -0800

    fix: updated homebrew setup

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -3,7 +3,14 @@ echo "\n*️⃣  setting up homebrew"
 # install homebrew
 if [[ ! -x "/opt/homebrew/bin/brew" && ! -x "/usr/local/bin/brew" ]]; then
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "🔧 installed homebrew ✅"
+  
+  if [[ $(uname -m) == 'arm64' ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    echo "🔧 installed homebrew (apple silicon) ✅"
+  else
+    echo "🔧 installed homebrew (intel) ✅"
+  fi
+
 else
   echo "🔧 homebrew already installed ⏭️"
 fi
