553b14ed378e5174e31f1bed987e78c73b1882cf
Jon Huber <hubermjonathan@gmail.com>
2022-11-17 11:37:02 -0800

    fix: added prompt back

--- zsh/shared.zsh
+++ zsh/shared.zsh
@@ -1,3 +1,15 @@
+# prompt
+function git_branch() {
+  branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+  if [[ $branch == "" ]]; then
+    echo " "
+  else
+    echo " ($branch) "
+  fi
+}
+setopt prompt_subst
+PROMPT='%~$(git_branch)%F{40}-->%f '
+
 # tab renaming
 tab() {
   echno -ne "\033]0;$*\007"
