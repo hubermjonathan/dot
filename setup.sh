37e1acf6f41c25b149d9d3226670fd85fda4c049
Jon Huber <hubermjonathan@gmail.com>
2022-11-30 11:23:36 -0800

    feat: added confirmations for each setup step

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 # load setup files
 for setup in */; do
-  source "${setup}setup.sh"
+  read -p "set up ${setup%?}? " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    source "${setup}setup.sh"
+  else
+    echo
+  fi
 done
 
 unset setup
